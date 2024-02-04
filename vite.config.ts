528e6b93662b22ac0e58a9211fb8aad935405a45
九日 <mail@sumiler.com>
2024-02-04 22:10:04 +0800

    feat:vitepress

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
